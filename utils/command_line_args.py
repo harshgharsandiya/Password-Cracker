2ee3b9dcf86bacfd2a48a3b581bc1d274a129aac
harshgharsandiya <harshgharsandiya@gmail.com>
2024-12-15 12:45:00 +0530

    Custom Wordlist

--- utils/command_line_args.py
+++ utils/command_line_args.py
@@ -10,6 +10,15 @@ def parse_arg():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-w", "--word", type=str, help="Single password", default=None)
     group.add_argument("-W", "--wordlist", type=str, help="Path to the Wordlist File", default=None)
+    group.add_argument("-cw" "--custom-wordlist", action="store_true", help="Generate custom wordlist")
+    
+    parser.add_argument("--charset", type=str, default="a-z,0-9",
+        help="Character set for custom wordlist (e.g., a-z, A-Z, 0-9, all, or custor char like abc123)")
+    parser.add_argument("--min-length", type=int, default=1, help="Minimum password lenght")
+    parser.add_argument("--min-length", type=int, default=3, help="Maximum password lenght")
+    parser.add_argument("--first-char-rule", type=str, default=None, help="First character rule (e.g., A-Z)")
+    parser.add_argument("--no-repeat", action="store_true", help="Password with no repeat character")
+    parser.add_argument("--exclude-chars", type=str, default=None, help="Comma-seperated list of char to exclude (e.g., !,@,#)")
     
     #Optional Arguments
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose Outpue")
---
+++ wordlist_generator/wordlist_generator.py
@@ -0,0 +1,62 @@
+import itertools
+import string
+
+def parse_charset(charset_string):
+    """
+    return appropriate character set
+    """
+    charsets = {
+        "a-z": string.ascii_lowercase,
+        "A-Z": string.ascii_uppercase,
+        "0-9": string.digits,
+        "all": string.ascii_letters + string.digits
+    }
+    
+    charset = []
+    for i in charset_string.split(','):
+        if i in charsets:
+            charset.append(charsets[i])
+        else:
+            #user custom sets (e.g., "abc123")
+            charset.append(i)
+            
+    return ''.join(charset)
+
+def generate_custom_wordlist(charset, min_length, max_length, first_char_rule=None, no_repeats=False, exclude_chars=None):
+    """
+    Based on specified rules generate wordlists
+    """
+    wordlist = []
+    first_char_set = charset if first_char_rule is None else first_char_rule
+    
+    #Generate passwords
+    for length in range(min_length, max_length+1):
+        
+        if length == 1:
+            wordlist.extend([''.join(p) for p in itertools.product(charset, repeat=length)])
+        else:
+            for first_char in first_char_set:
+                for rest in itertools.product(charset, repeat=length - 1):
+                    password = first_char + ''.join(rest)
+                    
+                    #additional rule
+                    if no_repeats and len(set(password)) != len(password):
+                        continue
+                    if exclude_chars:
+                        if any(c in password for c in exclude_chars):
+                            continue
+                    
+                    wordlist.append(password)
+                    
+        
+    return wordlist
+
+#Driver Code
+if __name__ == "__main__":
+    c = parse_charset("a-z,A-Z,34")
+    w = generate_custom_wordlist(c, 1, 2, "az", True, ['1','z'])
+    with open("wordlist.txt", "w") as file:
+        for word in w:
+            file.write(word + "\n")
+    print(len(w))
+            
